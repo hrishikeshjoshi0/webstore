3802cc770905a66fea2c3493a502cd286209a5c5
nachiket <Tanmay@10.126.250.174>
2013-02-05 20:29:41 +0530

    URL mapping changes...

--- webstore/grails-app/conf/UrlMappings.groovy
+++ webstore/grails-app/conf/UrlMappings.groovy
@@ -9,6 +9,7 @@ class UrlMappings {
 	
 			"/"(view:"/index")
 			"500"(view:'/error')
+			"404"(view:'/error')
 			
 			"/login/$action?"(controller: "login")
 			"/logout/$action?"(controller: "logout")
